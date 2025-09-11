fd090158c33247ba749495481b017cc38fbfafa9
blur88 <blur8809@yahoo.com>
2025-09-11 21:34:40 +0800

    remove calculator

--- frontend/src/components/calculator/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CalculatorCore } from './CalculatorCore'
-export { default as CalculatorDialog } from './CalculatorDialog'
-export { default as SlidingCalculatorPanel } from './SlidingCalculatorPanel'
