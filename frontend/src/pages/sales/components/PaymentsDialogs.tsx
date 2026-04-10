56db46c4dd3d8285a44aa3baf755a93264dd350b
blur88 <blur8809@yahoo.com>
2026-04-10 23:57:24 +0800

    feat: add PaymentsDialogs component

---
+++ frontend/src/pages/sales/components/PaymentsDialogs.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import type { PaymentListItem } from '../hooks/usePaymentsPageState'
+
+import { PaymentReceiptPrint } from '@/components/print'
+import DeletedPaymentsDialog from '@/components/sales/DeletedPaymentsDialog'
+
+interface PaymentsDialogsProps {
+  deletedPaymentsDialogOpen: boolean
+  printDialogOpen: boolean
+  selectedPayment: PaymentListItem | null
+  onCloseDeletedPaymentsDialog: () => void
+  onClosePrintDialog: () => void
+}
+
+const PaymentsDialogs: React.FC<PaymentsDialogsProps> = ({
+  deletedPaymentsDialogOpen,
+  printDialogOpen,
+  selectedPayment,
+  onCloseDeletedPaymentsDialog,
+  onClosePrintDialog,
+}) => {
+  return (
+    <>
+      <DeletedPaymentsDialog open={deletedPaymentsDialogOpen} onClose={onCloseDeletedPaymentsDialog} />
+      {selectedPayment && (
+        <PaymentReceiptPrint
+          open={printDialogOpen}
+          onClose={onClosePrintDialog}
+          payment={selectedPayment as any}
+        />
+      )}
+    </>
+  )
+}
+
+export default PaymentsDialogs
