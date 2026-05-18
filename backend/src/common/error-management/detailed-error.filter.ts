db31dd4599a0546f0f02e837b3fa57652266bb3d
blur88 <blur8809@yahoo.com>
2026-05-18 18:51:02 +0800

    feat(error-management): move DetailedErrorFilter

---
+++ backend/src/common/error-management/detailed-error.filter.ts
@@ -0,0 +1,39 @@
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(BadRequestException)
+export class DetailedErrorFilter implements ExceptionFilter {
+  catch(exception: BadRequestException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    const exceptionResponse = exception.getResponse();
+
+    if (typeof exceptionResponse === 'object' && exceptionResponse !== null) {
+      const detailedResponse = exceptionResponse as any;
+      if (
+        detailedResponse.error &&
+        (detailedResponse.error === 'DELETION_PREVENTED_BY_DEPENDENCIES' ||
+          detailedResponse.error === 'PERMANENT_DELETE_PREVENTED_BY_DEPENDENCIES')
+      ) {
+        return response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          method: request.method,
+          ...detailedResponse,
+        });
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      message: typeof exceptionResponse === 'string' ? exceptionResponse : (exceptionResponse as any).message,
+    });
+  }
+}
