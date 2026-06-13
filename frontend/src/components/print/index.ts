6e1ba7595b32a8dc8f7cee7a83878295ec906269
blur88 <blur8809@yahoo.com>
2026-06-14 00:29:10 +0800

    refactor(purchasing): remove standalone PO/vendor-payment print components

--- frontend/src/components/print/index.ts
+++ frontend/src/components/print/index.ts
@@ -1,3 +1,1 @@
 export { default as InvoicePrint } from './InvoicePrint'
-export { default as PurchaseOrderPrint } from './PurchaseOrderPrint'
-export { default as VendorPaymentPrint } from './VendorPaymentPrint'
